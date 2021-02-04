6deaf915709c26fb5f6190cd91806e002bdf1af8
roowahoo <tohyinru95@gmail.com>
2021-02-04 08:09:36 +0000

    added map

---
+++ Maps/script.js
@@ -0,0 +1,16 @@
+let london = [ 52.2053,0.1218]; // #1 Singapore latlng
+let map = L.map('london-map').setView(london, 13); // #2 Set the center point
+
+// setup the tile layers
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
+}).addTo(map);
+
+let cambridgeMarker= L.marker([52.2122,0.1549]);
+cambridgeMarker.addTo(map);
+cambridgeMarker.bindPopup("<p>Abbey Stadium</p>")
